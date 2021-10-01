edb84f723acda78aaa8063120cc8c777caa64b98
Robson Medeiros <robson.medeiros@ufrpe.br>
2021-10-01 08:38:25 -0300

    Create ServidorRPC.java

---
+++ servidor-rpc/src/main/java/br/ufrpe/dc/servidorrpc/ServidorRPC.java
@@ -0,0 +1,49 @@
+package br.ufrpe.dc.servidorrpc;
+
+import java.io.IOException;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcServer;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+import org.apache.xmlrpc.webserver.WebServer;
+
+public class ServidorRPC {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Iniciando o servidor........");
+		
+		WebServer server = new WebServer(8080);
+		XmlRpcServer xmlRpcServer = server.getXmlRpcServer();
+		PropertyHandlerMapping phm = new PropertyHandlerMapping();
+		
+		
+		try {
+			phm.addHandler("Calculadora", Calculadora.class );
+			xmlRpcServer.setHandlerMapping(phm);
+			
+			
+			XmlRpcServerConfigImpl serverConfig =
+		              (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
+		          serverConfig.setEnabledForExtensions(true);
+		          serverConfig.setContentLengthOptional(false);
+			
+			server.start();
+			
+			System.out.println("Servidor inicializado.");
+			System.out.println("Aguardando requisições.");
+			
+		} catch (IOException | XmlRpcException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	
+
+}
