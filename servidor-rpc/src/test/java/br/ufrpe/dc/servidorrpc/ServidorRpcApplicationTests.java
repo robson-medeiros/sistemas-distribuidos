e0a2f3ea03f14b3dfb556d5b8534c63bcc4a864c
Robson Medeiros <robson.medeiros@ufrpe.br>
2021-10-01 08:38:29 -0300

    Create ServidorRpcApplicationTests.java

---
+++ servidor-rpc/src/test/java/br/ufrpe/dc/servidorrpc/ServidorRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrpe.dc.servidorrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
