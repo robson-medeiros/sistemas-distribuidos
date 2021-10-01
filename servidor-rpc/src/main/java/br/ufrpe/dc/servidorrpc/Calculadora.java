d08c86103daf41f9af9726bedac80fcda3c34970
Robson Medeiros <robson.medeiros@ufrpe.br>
2021-10-01 08:38:23 -0300

    Create Calculadora.java

---
+++ servidor-rpc/src/main/java/br/ufrpe/dc/servidorrpc/Calculadora.java
@@ -0,0 +1,21 @@
+package br.ufrpe.dc.servidorrpc;
+
+public class Calculadora {
+	
+	public float somar(float x, float y) {
+		return x + y;
+	}
+	
+	public float multiplicar(float x, float y) {
+		return x * y;
+	}
+	
+	public float subtrair(float x, float y) {
+		return x - y;
+	}
+	
+	public float dividir(float x, float y) {
+		return x / y;
+	}
+
+}
