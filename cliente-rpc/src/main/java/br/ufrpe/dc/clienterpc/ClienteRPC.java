578432520d2ec5a287f9b6c70a6c4ee7e99b9cfd
Robson Medeiros <robson.medeiros@ufrpe.br>
2021-10-01 08:38:50 -0300

    Create ClienteRPC.java

---
+++ cliente-rpc/src/main/java/br/ufrpe/dc/clienterpc/ClienteRPC.java
@@ -0,0 +1,33 @@
+package br.ufrpe.dc.clienterpc;
+
+  import java.net.URL;
+
+  import org.apache.xmlrpc.client.XmlRpcClient;
+  import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+  import org.apache.xmlrpc.client.XmlRpcCommonsTransportFactory;
+
+  public class ClienteRPC {
+      public static void main(String[] args) throws Exception {
+          // Configurações
+          XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+          config.setServerURL(new URL("http://localhost:8080/xmlrpc"));
+          config.setEnabledForExtensions(true); // Abilita  valores float
+          
+
+          XmlRpcClient client = new XmlRpcClient();
+        
+          // Usa Commons HttpClient como transporte
+          client.setTransportFactory(
+              new XmlRpcCommonsTransportFactory(client));
+          // set configuration
+          client.setConfig(config);
+
+          // Faz uma chamada remota
+          Object[] params = new Object[]
+              { new Float(2.0f), new Float(3.0f) };
+          Float result = (Float) client.execute("Calculadora.somar", params);
+          System.out.println("2 + 3 = " + result);
+        
+          
+      }
+  }
