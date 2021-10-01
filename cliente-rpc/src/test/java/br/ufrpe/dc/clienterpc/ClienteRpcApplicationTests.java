8ebde61bdbca15a8679c22b36394937f05cfb3ab
Robson Medeiros <robson.medeiros@ufrpe.br>
2021-10-01 08:38:45 -0300

    Create ClienteRpcApplicationTests.java

---
+++ cliente-rpc/src/test/java/br/ufrpe/dc/clienterpc/ClienteRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrpe.dc.clienterpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
